dd7077ac40ca913cfb00b7c1d740e61edbd8a2fa
cleog <14929046+cleog@users.noreply.github.com>
2024-02-03 16:50:01 -0800

    Added quiz code v1

--- MajorExplNextJS/src/app/quiz/page.js
+++ MajorExplNextJS/src/app/quiz/page.js
@@ -1,108 +1,74 @@
 'use client'
-
-import Image from "next/image";
-import quiz from '/src/components/quiz.png'
 import { useState } from 'react'
+import questions from './questions.json'
 
 export default function Home() {
+  // Index of current question
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  // Array of objects with answer by user
+  const [selectedOptions, setSelectedOptions] = useState([]);
 
-  const sayHello = (event) => {
-    alert("Hello")
-    ;
+  const handleAnswerOption = (answer) => {
+    setSelectedOptions([
+      (selectedOptions[currentQuestion] = { answerByUser: answer }),
+    ]);
+    setSelectedOptions([...selectedOptions]);
   };
 
+  const handleNext = () => {
+    if (currentQuestion < questions.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (currentQuestion > 0) {
+      setCurrentQuestion(currentQuestion - 1);
+    }
+  };
+
+  const handleSubmit = () => {
+    alert('Quiz submitted! Your answers were: ' + JSON.stringify(selectedOptions.map((o) => o.answerByUser)));
+    // Add my logic here
+  };
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+    <main className="flex min-h-screen flex-col p-24">
+      <div>
+        <h1 className="mb-3 text-2xl text-center mt-10 font-semibold">Major Exploration Quiz</h1>
+        <h2 className="mb-3 text-xl text-center mt-10">This quiz will recommend 3 College of Engineering majors that you may be a good fit for!</h2>
+        <div className="flex flex-col mt-20 items-start w-full outline p-4 outline-orange/50 bg-slate-100/40">
+          <h4 className="mt-10 text-xl text-black/60">Question {currentQuestion + 1} of {questions.length}</h4>
+          <div className="mt-4 text-2xl text-black">
+            {questions[currentQuestion].question}
+          </div>
+          <div className="mt-4 text-2xl text-black">
+            {questions[currentQuestion].answerOptions.map((option, i) => (
+              <div key={i}>
+                <input 
+                  type="radio" 
+                  id={`${currentQuestion}-${i}`} 
+                  name={`question-${currentQuestion}`}
+                  onChange={(e) => handleAnswerOption(option.answer)}
+                  checked={
+                    option.answer === selectedOptions[currentQuestion]?.answerByUser
+                  }
+                  value={option.answer} 
+                  />
+                <label htmlFor={`${currentQuestion}-${i}`}>{option.answer}</label>
+              </div>
+            ))}
+          </div>
+          <span>
+            <button className="bg-orange text-white mr-4" onClick={handlePrevious}>Previous</button>
+            {currentQuestion === questions.length - 1 ? (
+              <button className="bg-orange text-white" onClick={handleSubmit}>Submit</button>
+            ) : (
+              <button className="bg-orange text-white" onClick={handleNext}>Next</button>
+            )}
+          </span>
+        </div>
       </div>
-
-      <div id="wrapper">
-        <h1>What kind of College of Engineering major might work for you?</h1>
-        <br />
-
-        <form id="quiz">
-          {/* Question 01 */}
-          <h2>What do you value most when it comes to a career?</h2>
-          
-          {/* Question 1 answers */}
-          <label><input type="radio" name="q1" value="c1"/>
-          Financial security
-          </label><br />
-          <label><input type="radio" name="q1" value="c2"/>
-          Helping others
-          </label><br />
-          <label><input type="radio" name="q1" value="c3"/>
-          Job stability
-          </label><br />
-          <label><input type="radio" name="q1" value="c4"/>
-          Developing technologies
-          </label><br />
-          <br />
-
-          {/* Question 02 */}
-          <h2>What is your ideal work environment?</h2>
-  
-          {/* Question 02 Answers */}
-          <label><input type="radio" name="q2" value="c1"/>
-          A laboratory
-          </label><br />
-          <label><input type="radio" name="q2" value="c2"/>
-          A place with high levels of appreciation
-          </label><br />
-          <label><input type="radio" name="q2" value="c3"/>
-          A place with cutting edge equipment
-          </label><br />
-          <label><input type="radio" name="q2" value="c4"/>
-          An informal environment such as from home
-          </label><br />
-          <br />
-    
-          {/* Question 03 */}
-          <h2>Who inspires you?</h2>
-  
-          {/* Question 03 Answers*/}
-          <label><input type="radio" name="q3" value="c1"/>
-          Developers of new technologies
-          </label><br />
-          <label><input type="radio" name="q3" value="c2"/>
-          People who sacrifice for the greater good
-          </label><br />
-          <label><input type="radio" name="q3" value="c3"/>
-          Creative or artistic people
-          </label><br />
-          <label><input type="radio" name="q3" value="c4"/>
-          Innovative people
-          </label><br />
-          <br />
-    
-          {/* Question 04 */}
-          <h2>What do you do when you encounter a difficult problem?</h2>
-  
-          {/* Question 04 Answers */}
-          <label><input type="radio" name="q4" value="c1"/>
-          Research potential solutions independently
-          </label><br />
-          <label><input type="radio" name="q4" value="c2"/>
-          Ask for help
-          </label><br />
-          <label><input type="radio" name="q4" value="c3"/>
-          Step away from the problem to relax and gain perspective
-          </label><br />
-          <label><input type="radio" name="q4" value="c4"/>
-          Experiment with potential solutions until one is successful
-          </label><br />
-          <br />
-
-          {/* Buttons */}
-          <button name="submit" type="submit" id="submit" onClick={sayHello}>Submit Your Answers</button>
-          <button name="reset" type="reset" id="reset" onClick={sayHello}>Reset</button>
-        </form>
-  
-        <div id="answer">Result</div>
-      </div>
-
     </main>
   )
 }
